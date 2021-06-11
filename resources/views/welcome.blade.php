2797ec9a32aeac263b08325d806f9b21e9d06bab
Hyung97 <tammyhyunglee@gmail.com>
2021-06-10 21:11:07 -0600

    modificaciones

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
 
 <body>
     <div class='menu'>
-        <nav class="navbar navbar-light navbar-expand-lg" style="background-color: #01918E; border:2px solid Teal;">
+        <nav class="navbar navbar-light navbar-expand-lg" style="background-color: #2EFE9A; border:2px solid Teal;">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">INTEGRACION DIGITAL</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,7 @@ Route::get('/segundo/noverbal', [App\Http\Controllers\MenuController::class, 'no
 
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
